6b16ccf5629e0b25561ca14810933a79951b7373
ajaybarath <ajaybarath3@gmail.com>
2021-11-07 18:48:35 +0530

    Refractored: get mappping with service class

--- src/main/java/com/bridgelabz/springmessagingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/springmessagingapp/controller/GreetingController.java
@@ -5,24 +5,23 @@ import com.bridgelabz.springmessagingapp.model.Greeting;
 import com.bridgelabz.springmessagingapp.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
-    private static final String template = "Hello %s !";
-    private final AtomicLong counter = new AtomicLong();
+    @Autowired
+    IGreetingService greetingService;
 
-
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public Greeting greeting(@RequestParam (value = "firstName", defaultValue = "World") String firstName, @RequestParam (value = "lastName", defaultValue = "World") String lastName) {
-        User user = new User(firstName, lastName);
-
-        return new Greeting(counter.incrementAndGet(), user.toString());
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return greetingService.addGreeting(user);
     }
 
 }
