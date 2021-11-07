8354601be9b83054cfe0800c0cbc27ce0af7f6cb
ajaybarath <ajaybarath3@gmail.com>
2021-11-07 16:41:50 +0530

    Refractored: the controller to pass the user object

--- src/main/java/com/bridgelabz/springmessagingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/springmessagingapp/controller/GreetingController.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.springmessagingapp.controller;
 
+import com.bridgelabz.springmessagingapp.implementation.IGreetingService;
 import com.bridgelabz.springmessagingapp.model.Greeting;
+import com.bridgelabz.springmessagingapp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +17,12 @@ public class GreetingController {
     private static final String template = "Hello %s !";
     private final AtomicLong counter = new AtomicLong();
 
+
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam (value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template ,name));
+    public Greeting greeting(@RequestParam (value = "firstName", defaultValue = "World") String firstName, @RequestParam (value = "lastName", defaultValue = "World") String lastName) {
+        User user = new User(firstName, lastName);
+
+        return new Greeting(counter.incrementAndGet(), user.toString());
     }
 
 }
