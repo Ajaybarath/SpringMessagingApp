fe3b08a78528d0f84d0069f85b38c6d93ae125bf
ajaybarath <ajaybarath3@gmail.com>
2021-11-07 20:31:08 +0530

    Added: get method to find by id

--- src/main/java/com/bridgelabz/springmessagingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/springmessagingapp/controller/GreetingController.java
@@ -4,10 +4,9 @@ import com.bridgelabz.springmessagingapp.implementation.IGreetingService;
 import com.bridgelabz.springmessagingapp.model.Greeting;
 import com.bridgelabz.springmessagingapp.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/greeting")
@@ -22,6 +21,11 @@ public class GreetingController {
         user.setFirstName(firstName);
         user.setLastName(lastName);
         return greetingService.addGreeting(user);
+    }
+
+    @GetMapping("/get/{id}")
+    public Optional<Greeting> getGreeting(@PathVariable("id") long id) {
+        return greetingService.getGreetingById(id);
     }
 
 }
